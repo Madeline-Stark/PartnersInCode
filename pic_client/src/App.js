673e017fa8e048efbfdf847f2364ef4cded95588
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-01 14:51:49 -0800

    Move app code from old branch

--- pic_client/src/App.js
+++ pic_client/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-
+import Programmers from './components/Programmers';
+import ProgrammerService from './services/ProgrammerService';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      programmers: []
+    }
+  }
+
+  componentDidMount() {
+    ProgrammerService.fetchProgrammers()
+  }
+
   render() {
     return (
+      <Programmers programmers={programmers}/>
     );
   }
 }
