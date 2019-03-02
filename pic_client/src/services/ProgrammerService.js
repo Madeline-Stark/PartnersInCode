0b65f67777654c4d6c25757cf3dc3fd7e90e0a80
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-01 18:32:31 -0800

    Add createProgrammer

--- pic_client/src/services/ProgrammerService.js
+++ pic_client/src/services/ProgrammerService.js
@@ -5,6 +5,18 @@ const ProgrammerService = {
     return fetch(`${API_URL}/programmers`)
       .then(response => response.json())
   }
+
+  createProgrammer(programmer) {
+    const request = {
+      method: 'POST',
+      body: JSON.stringify(programmer),
+      contentType: 'application/json',
+      accepts: 'application/json'
+    }
+
+    return fetch(`${API_URL}/programmers`, request)
+      .then(response => response.json())
+  }
 }
 
 export default ProgrammerService;
