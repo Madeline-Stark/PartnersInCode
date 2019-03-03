0791c2bc70bea9352b9f91cf656646223ff5a549
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-02 18:19:26 -0800

    Create basic programmers reducer

--- pic_client/src/store.js
+++ pic_client/src/store.js
@@ -4,3 +4,13 @@ import {
   combineReducers
 } from 'redux';
 import thunk from 'redux-thunk';
+
+const programmersReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'GET_PROGRAMMER_SUCCESS':
+      return action.programmer;
+
+    default:
+      return state;
+  }
+}
