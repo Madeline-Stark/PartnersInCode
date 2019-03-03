1ef6950e72d386499747b1f2cdefeb3a218d13b2
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-02 20:09:30 -0800

    Unregister

--- pic_client/src/index.js
+++ pic_client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import store from './store.js';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
@@ -14,4 +14,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'));
 
-registerServiceWorker();
+unregisterServiceWorker();
