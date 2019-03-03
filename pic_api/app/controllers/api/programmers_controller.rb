7476ee5a89c0aaaefb01cb98809f298e0cec7753
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-02 16:15:18 -0800

    Add except to routes

--- pic_api/app/controllers/api/programmers_controller.rb
+++ pic_api/app/controllers/api/programmers_controller.rb
@@ -1,6 +1,25 @@
 class Api::ProgrammersController < ApplicationController
+
+#don't need new or edit-handled on server side
+
   def index
     @programmers = Programmer.all
     render json: @programmers
   end
+
+  def create
+    @programmer = Programmer.new(programmer_params)
+    if @programmer.save
+      render json: @programmer
+    else
+      render json: { errors: {message: 'Problem saving, try again.'}}
+    end
+  end
+
+  private
+
+  def programmer_params
+    params.require(:programmer).permit(:name, :email, :languages, :time_zone)
+  end
+
 end
--- pic_api/config/routes.rb
+++ pic_api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :programmers
+    resources :programmers, except: [:new, :edit]
   end
 end
