d14975b1279088de72264d5f1991d255ddad5711
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-02 16:25:45 -0800

    Add show, update, and destroy to controller

--- pic_api/app/controllers/api/programmers_controller.rb
+++ pic_api/app/controllers/api/programmers_controller.rb
@@ -1,5 +1,5 @@
 class Api::ProgrammersController < ApplicationController
-
+before_action :set_programmer, only: [:show, :edit, :destroy]
 #don't need new or edit-handled on server side
 
   def index
@@ -16,7 +16,31 @@ class Api::ProgrammersController < ApplicationController
     end
   end
 
+  def show
+    render json: @programmer
+  end
+
+  def update
+    if @programmer.update(programmer_params)
+      render json: @programmer
+    else
+      render json: { errors: {message: 'Problem saving, try again.'}}
+    end
+  end
+
+  def destroy
+    if @programmer.destroy
+      render json: { message: 'Deleted forever!'}
+    else
+      render json: { errors: {message: 'Problem deleting, try again.'}}
+    end
+  end
+
   private
+
+  def set_programmer
+    @programmer = Programmer.find_by(id: params[:id])
+  end
 
   def programmer_params
     params.require(:programmer).permit(:name, :email, :languages, :time_zone)
