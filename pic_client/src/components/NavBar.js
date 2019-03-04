0de578f7b9e0105e1b825355d492a08fab5111ad
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-03 17:23:52 -0800

    Add breaks to navbar

--- pic_client/src/components/NavBar.js
+++ pic_client/src/components/NavBar.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink to="/">About</NavLink>
-      <NavLink to="/programmers">Programmers</NavLink>
-      <NavLink to="/resources">Resources</NavLink>
+      <NavLink to="/">About</NavLink><br />
+      <NavLink to="/programmers">Programmers</NavLink><br />
+      <NavLink to="/resources">Resources</NavLink><br />
     </div>
   );
 };
