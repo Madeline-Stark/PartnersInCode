485447e0122f01c543e029d498c0a1f717691db7
Madeline-Stark <madeline.stark1@gmail.com>
2019-03-03 18:38:23 -0800

    Add css so columns lay side by side

--- pic_client/src/components/ProgrammerCard.js
+++ pic_client/src/components/ProgrammerCard.js
@@ -3,14 +3,12 @@ import '../containers/Programmers.css';
 
 const ProgrammerCard = ({ programmer }) => (
   <div class="column">
-  <div class="card">
   <div key={programmer.id} className="ProgrammerCard">
   <img className="ProgrammerImage" src={programmer.img_url} alt={programmer.name} />
   <h3>{programmer.name}</h3>
     <p>Programming Languages: {programmer.languages}</p>
     <p>Time Zone: {programmer.time_zone}</p>
     <p>Email: {programmer.email}</p>
-  </div>
   </div>
   </div>
 )
